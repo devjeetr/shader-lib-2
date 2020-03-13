3940a0cac43bc790253de9f3d6af9811a45c1709
Devjeet Roy <devjeetrr@gmail.com>
2020-03-12 19:04:24 -0700

    Moved demos out to a separate folder

---
+++ src/js/demos/instanced_trianges.ts
@@ -0,0 +1,75 @@
+import { DrawConfigTypes } from "../GPX/primitives";
+import { createApp } from "../GPX/shader";
+
+export const instanced_triangles_demo = (gl: WebGL2RenderingContext) => {
+  const draw = createApp({
+    gl,
+    vertexShader: `#version 300 es
+      
+      // an attribute is an input (in) to a vertex shader.
+      // It will receive data from a buffer
+      in vec4 a_position;
+      in vec4 a_offset;
+      // all shaders have a main function
+      void main() {
+       
+        // gl_Position is a special variable a vertex shader
+        // is responsible for setting
+        gl_Position = a_position + a_offset;
+      }`,
+    fragmentShader: `#version 300 es
+   
+      // fragment shaders don't have a default precision so we need
+      // to pick one. mediump is a good default. It means "medium precision"
+      precision mediump float;
+       
+      // we need to declare an output for the fragment shader
+      out vec4 outColor;
+       
+      void main() {
+        // Just set the output to a constant reddish-purple
+        outColor = vec4(1, 0, 0.5, 1);
+      }`,
+    attributes: [
+      {
+        name: "a_position",
+        target: gl.ARRAY_BUFFER,
+        data: (_, { data }) => {
+          return new Float32Array(data);
+        },
+        size: 2,
+        type: gl.FLOAT,
+        normalized: false,
+        stride: 0,
+        offset: 0,
+        usage: gl.STATIC_DRAW
+      },
+      {
+        name: "a_offset",
+        target: gl.ARRAY_BUFFER,
+        data: (_, { offset }) => {
+          return new Float32Array(offset);
+        },
+        size: 2,
+        type: gl.FLOAT,
+        normalized: false,
+        stride: 0,
+        offset: 0,
+        usage: gl.DYNAMIC_DRAW,
+        divisor: 1
+      }
+    ],
+    draw: {
+      kind: DrawConfigTypes.drawArraysInstanced,
+      type: gl.TRIANGLES,
+      first: 0,
+      indexCount: 3,
+      instanceCount: 5
+    }
+  });
+
+  draw({
+    data: [0, 0, 0, 0.5, 0.3, 0],
+    offset: [0.3, 0.2, -0.9, -0.2, -0.2, -0.1, 0.5, 0.2, -0.4, -0.2]
+  });
+};
--- src/js/index.ts
+++ src/js/index.ts
@@ -1,5 +1,7 @@
 import { DrawConfigTypes } from "./GPX/primitives";
 import { createApp } from "./GPX/shader";
+import { hello_world_demo } from "./demos/hello_world";
+import { instanced_triangles_demo } from "./demos/instanced_trianges";
 
 const canvas = document.createElement("canvas");
 
@@ -10,64 +12,5 @@ gl.canvas.height = 500;
 gl.canvas.width = 500;
 canvas.style.height = "500px";
 canvas.style.width = "500px";
-const drawTriangle = createApp({
-  gl,
-  vertexShader: `#version 300 es
-    
-    // an attribute is an input (in) to a vertex shader.
-    // It will receive data from a buffer
-    in vec4 a_position;
-    uniform vec4 u_offset;
-    // all shaders have a main function
-    void main() {
-     
-      // gl_Position is a special variable a vertex shader
-      // is responsible for setting
-      gl_Position = a_position + u_offset;
-    }`,
-  fragmentShader: `#version 300 es
- 
-    // fragment shaders don't have a default precision so we need
-    // to pick one. mediump is a good default. It means "medium precision"
-    precision mediump float;
-     
-    // we need to declare an output for the fragment shader
-    out vec4 outColor;
-     
-    void main() {
-      // Just set the output to a constant reddish-purple
-      outColor = vec4(1, 0, 0.5, 1);
-    }`,
-  attributes: [
-    {
-      name: "a_position",
-      target: gl.ARRAY_BUFFER,
-      data: (_, { data }) => {
-        return new Float32Array(data);
-      },
-      size: 2,
-      type: gl.FLOAT,
-      normalized: false,
-      stride: 0,
-      offset: 0,
-      usage: gl.STATIC_DRAW
-    }
-  ],
-  uniforms: [
-    {
-      name: "u_offset",
-      data: (_, { offset }) => {
-        return offset;
-      },
-      type: "uniform4fv"
-    }
-  ],
-  draw: {
-    kind: DrawConfigTypes.drawArrays,
-    type: gl.TRIANGLES,
-    first: 0,
-    indexCount: 3,
-  }
-});
 
-drawTriangle({ data: [0, 0, 0, 0.5, 0.3, 0], offset: [0.3, 0.2, 0, 0] });
+instanced_triangles_demo(gl);
